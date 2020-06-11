ae856a71d66fc0037692633fe5e20f7875a65d65
Vinicius Avansini <vi.avn@hotmail.com>
2020-06-11 14:29:46 -0300

    Desafio 04 - refatoração de código

--- challenges/shopping-cart/src/index.js
+++ challenges/shopping-cart/src/index.js
@@ -1,77 +1,78 @@
-// const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 function getShoppingCart(ids, productsList) {
+  // Dado uma lista de produtos, recuperar seus nomes e categorias
+  const categoriesSet = new Set();
+  const filteredProducts = productsList.reduce((accumulator, current) => {
+    if (ids.includes(current.id)) {
+      // Separando as categorias
+      categoriesSet.add(current.category);
 
-	// Dado uma lista de produtos, recuperar seus nomes e categorias
-	const filterFn = filterProduct(ids);
-	const filteredProducts = productsList.filter(filterFn);
+      return [...accumulator, current];
+    }
 
-	// Separando as categorias
-	const mySet = new Set();
-	filteredProducts.forEach(p => mySet.add(p.category));
+    return accumulator;
+  }, []);
 
-	let promotion = size[mySet.size];
-	promotion = !!promotion ? promotion() : 'FULL LOOK';
+  const promotion = promotions[categoriesSet.size - 1];
+  const totals = calculateTotals(filteredProducts, promotion);
 
-	const totals = calculateTotals(filteredProducts, promotion);
-	const values = calculateValues(totals.totalWithoutDiscount, totals.totalWithDisccount);
+  const products = filteredProducts.map((product) => {
+    const { name, category } = product;
+    return { name, category };
+  });
 
-	const products = filteredProducts.map(mapProducts)
-	return { products, promotion, totalPrice: `${totals.totalWithDisccount}`, ...values };
+  return {
+    products,
+    promotion,
+    ...totals,
+  };
 }
-
-const size = {
-	1: () => 'SINGLE LOOK',
-	2: () => 'DOUBLE LOOK',
-	3: () => 'TRIPLE LOOK'
-};
 
 const calculateTotals = (products, promotion) => {
-	const totalWithoutDiscount = products.reduce(getTotalValueWithoutDiscount, 0);
-	const totalWithoutDiscountFn = getTotalWithDisccount(promotion);
-	const totalWithDiscount = products.reduce(totalWithoutDiscountFn, 0);
+  const totalWithoutDiscount = products.reduce(
+    (accumulator, curr) => accumulator + curr.regularPrice,
+    0
+  );
+  const totalDiscountFn = totalWithDisccountReducer(promotion);
+  const totalWithDiscount = products.reduce(totalDiscountFn, 0);
 
-	return {
-		totalWithoutDiscount,
-		totalWithDisccount: totalWithDiscount.toFixed(2)
-	};
+  const discountValues = calculateDiscountValues(
+    totalWithoutDiscount,
+    totalWithDiscount
+  );
+
+  return {
+    totalPrice: `${totalWithDiscount.toFixed(2)}`,
+    ...discountValues,
+  };
 };
 
-const calculateValues = (totalWithoutDiscount, totalWithDisccount) => {
-	let discountValue = calculateDiscountValue(totalWithoutDiscount, totalWithDisccount);
-	discountValue = `${discountValue.toFixed(2)}`;
+const calculateDiscountValues = (totalWithoutDiscount, totalWithDisccount) => {
+  const discountValue = totalWithoutDiscount - totalWithDisccount;
+  const discountPercentage = (
+    (discountValue * 100) /
+    totalWithoutDiscount
+  ).toFixed(2);
 
-	let discountPercentage = calculatePercentual(totalWithoutDiscount, discountValue);
-	discountPercentage = `${discountPercentage}%`;
-
-	return { discountValue, discount: discountPercentage };
+  return {
+    discountValue: `${discountValue.toFixed(2)}`,
+    discount: `${discountPercentage}%`,
+  };
 };
 
-const calculatePercentual = (totalWithoutDiscount, discountValue) => ((discountValue * 100) / totalWithoutDiscount).toFixed(2);
+const totalWithDisccountReducer = (promotionDesc) => {
+  return (acc, curr) => {
+    let val = 0;
+    const promotion = curr.promotions.find((x) =>
+      x.looks.includes(promotionDesc)
+    );
 
-const calculateDiscountValue = (val1, val2) => (val1 - val2);
+    if (promotion) val = promotion.price;
+    else val = curr.regularPrice;
 
-const getTotalValueWithoutDiscount = (accumulator, curr) => accumulator + curr.regularPrice;
-
-const getTotalWithDisccount = (promotionDesc) => {
-	return (acc, curr) => {
-		let val = 0;
-		const promotion = curr.promotions.find(x => x.looks.includes(promotionDesc));
-
-		if (promotion) val = promotion.price;
-		else val = curr.regularPrice;
-
-		return acc + val;
-	}
+    return acc + val;
+  };
 };
-
-const filterProduct = (ids) => {
-	return (product) => ids.includes(product.id);
-};
-
-const mapProducts = (product) => {
-	const { name, category } = product;
-	return { name, category };
-}
 
 module.exports = { getShoppingCart };
--- challenges/shopping-cart/src/start.js
+++ challenges/shopping-cart/src/start.js
@@ -1,9 +1,10 @@
 const { getShoppingCart } = require('./index');
+const { products } = require('../src/data/products');
 
 const singleLook = [110, 120, 130, 140];
 const doubleLook = [130, 140, 230, 260];
 const tripleLook = [110, 130, 140, 230, 310, 330];
 const fullLook = [120, 230, 310, 490];
 
-const products = getShoppingCart(tripleLook);
-console.log(products)
+const shoppingCart = getShoppingCart(singleLook, products);
+console.log(shoppingCart);
