4d41cb0e15488bb3b0a11e849337a0437d802e69
Vinicius Avansini <vi.avn@hotmail.com>
2020-06-16 23:09:05 -0300

    Desafio #07 - Desenvolvimento de Post

--- challenges/instagram-react/instagram-pt-1/src/components/Post/Post.jsx
+++ challenges/instagram-react/instagram-pt-1/src/components/Post/Post.jsx
@@ -4,7 +4,88 @@ import { Link } from 'react-router-dom';
 import './Post.scss';
 
 const Post = ({ postInfo, userInfo }) => {
-  return <article className="post"></article>;
+  const { name, avatar } = userInfo;
+  const { imageUrl, comments } = postInfo;
+  console.log(postInfo);
+
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  const handleFollowClick = () => setIsFollowing(!isFollowing);
+
+  function getPostCommentText() {
+    const comment = comments.slice(0, 1);
+
+    if (comment.length > 0) {
+      const firstComment = comment[0];
+      const commentsNumber = comments.length - 1;
+      
+      const commentsText =
+        commentsNumber === 1
+          ? `outra ${commentsNumber} pessoa`
+          : `outras ${commentsNumber} pessoas`;
+
+      return (
+        <>
+          curtido por <Link to="/">{firstComment.name}</Link> e{' '}
+          {`${commentsText}`}
+        </>
+      );
+    }
+
+    return '';
+  }
+
+  return (
+    <article className="post">
+      {/* BEGIN HEADER */}
+      <header className="post__header">
+        <div className="user">
+          <img
+            src={avatar}
+            alt={`Foto do usuário ${name}`}
+            className="user__thumb"
+          />
+          <Link className="user__name" to="/">
+            {name}
+          </Link>
+        </div>
+        <button
+          type="button"
+          className="post__context"
+          onClick={handleFollowClick}
+        >
+          <span className={`follow-btn ${isFollowing ? 'is-following' : ''}`}>
+            {isFollowing ? 'Seguindo' : 'Seguir'}
+          </span>
+        </button>
+      </header>
+      {/* END HEADER */}
+
+      {/* BEGIN POST IMAGE */}
+      <figure className="post__figure">
+        <img src={imageUrl} alt={`Publicação do usuário ${name}`} />
+      </figure>
+      {/* END POST IMAGE */}
+
+      {/* BEGIN CONTROLS */}
+      <footer className="post__controls">
+        <button className="post__control">
+          <i
+            aria-hidden="true"
+            className={`${liked ? 'fas' : 'far'} fa-heart`}
+          ></i>
+        </button>
+
+        <div className="post__status">
+          <div className="user">
+            <span>{getPostCommentText()}</span>
+          </div>
+        </div>
+      </footer>
+      {/* END CONTROLS */}
+    </article>
+  );
 };
 
 export default Post;
--- challenges/instagram-react/instagram-pt-1/src/routes/FeedRoute/FeedRoute.jsx
+++ challenges/instagram-react/instagram-pt-1/src/routes/FeedRoute/FeedRoute.jsx
@@ -13,13 +13,26 @@ const URL_USERS = `${BASE_URL}/users`;
 const FeedRoute = () => {
   const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const response = await fetch(URL_USERS);
-      const data = await response.json();
+      // const response = await fetch(URL_USERS);
+      // const data = await response.json();
+      // setUsers(data);
 
-      setUsers(data);
+      Promise.resolve([
+        {
+          avatar:
+            'https://viniciusvinna.netlify.app/assets//api-instagram/profiles/black-panther/black-panther-profile.jpg',
+          email: 'blackpanther@gmail.com',
+          id: '1',
+          name: "T'Challa",
+          username: 'blackpanther',
+        },
+      ]).then((value) => {
+        setUsers(value);
+      });
     };
 
     fetchUsers();
@@ -29,18 +42,63 @@ const FeedRoute = () => {
     const fetchPosts = async () => {
       const usersPosts = [];
 
+      setIsLoading(true);
+
       for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        const id = parseInt(user.id, 10);
+        // const user = users[i];
+        // const id = parseInt(user.id, 10);
 
-        const response = await fetch(`${URL_USERS}/${id}/posts`);
-        const posts = await response.json();
+        // const response = await fetch(`${URL_USERS}/${id}/posts`);
+        // const posts = await response.json();
+        // usersPosts.push(...posts);
 
-        usersPosts.push(...posts);
+        const res = await Promise.resolve([
+          {
+            id: '1',
+            imageUrl:
+              'https://i.ibb.co/HGhF4qd/Screen-Shot-2020-06-04-at-18-10-13.png',
+            userId: '1',
+            likes: [{ id: '1', postId: '1' }],
+            comments: [
+              {
+                avatar:
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/yayteejay/128.jpg',
+                comment:
+                  "programming the transmitter won't do anything, we need to override the back-end PNG bus!",
+                createdAt: '2020-03-27T00:58:06.401Z',
+                id: '1',
+                name: 'Santino Rowe',
+                postId: '1',
+              },
+              {
+                avatar:
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/kosmar/128.jpg',
+                comment:
+                  "I'll navigate the digital SCSI matrix, that should protocol the SAS pixel!",
+                createdAt: '2020-03-27T04:04:36.486Z',
+                id: '31',
+                name: 'Virginia Kshlerin',
+                postId: '1',
+              },
+              {
+                avatar:
+                  'https://viniciusvinna.netlify.app/assets//api-instagram/profiles/bruce/bruce-1.jpg',
+                comment:
+                  'The FTP capacitor is down, override the multi-byte monitor so we can copy the XML array!',
+                createdAt: '2020-06-04T07:15:00.786Z',
+                id: '68',
+                name: 'Oliver',
+                postId: '1',
+              },
+            ],
+          },
+        ]);
+
+        usersPosts.push(...res);
       }
 
-      console.log(usersPosts);
       setPosts(usersPosts);
+      setIsLoading(false);
     };
 
     fetchPosts();
@@ -56,7 +114,12 @@ const FeedRoute = () => {
 
   return (
     <div className="feed">
-      {<Posts posts={posts} getUserHandler={getUserHandler} />}
+      {/* <Stories /> */}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Posts posts={posts} getUserHandler={getUserHandler} />
+      )}
     </div>
   );
 };
