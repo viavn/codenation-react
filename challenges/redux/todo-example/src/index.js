13bddddb45afbd8cde4fd38e4e3bc9470508ca92
Vinicius Avansini <vi.avn@hotmail.com>
2020-06-28 18:50:30 -0300

    Redux Doc TodoApp exemplo

---
+++ challenges/redux/todo-example/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import App from './components/App';
+
+import './index.css';
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
